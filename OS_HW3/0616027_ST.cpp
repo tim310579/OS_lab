8606344aca30d31c2d8502b280d1d39544274ca5
tim310579 <43834434+tim310579@users.noreply.github.com>
2019-11-21 10:39:22 +0800

    ok

--- OS_HW3/0616027_ST.cpp
+++ OS_HW3/0616027_ST.cpp
@@ -1,7 +1,10 @@
 #include <iostream> 
 #include <stdlib.h>
-#define LARGE 100000
+#define LARGE 1000000
 using namespace std;
+
+int list[LARGE];
+int cnt = 0;
 /* function definitions */
 void sort(int list[]);
 
@@ -9,16 +12,30 @@ int main (int argc, const char * argv[])
 {
 	/* Use STDIN (e.g. scanf, cin) to take the input */
 	int input = 0;
-	int list[LARGE] ;
-	cin >> list[5];
-	for(int i =0; i < 100; i++){
-		cout << list[i]<<" ";
+	//int list[LARGE] = {0};
+	while(cin >> input){
+		list[cnt] = input;
+		cnt ++;
 	}
+	
+	sort(list);
+	
+	for(int i = 0; i < cnt; i++){
+		cout << list[i] <<" ";
+	}	
     return 0;
 }
 
 void sort(int list[])
 {
-
+	for(int i = 0; i < cnt; i++){
+		for(int j = i+1; j < cnt; j++){
+			if(list[i] > list[j]){
+				int tmp = list[i];
+				list[i] = list[j];
+				list[j] = tmp;	
+			}
+		}
+	}
 }
 
